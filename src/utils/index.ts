f2bd5ab61fbc17fa7208e83a56fc383f0ef2a8ea
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-07-08 02:50:01 +0700

    feat(util): add sweet alert util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,9 @@
+import alert from './sweet.alert';
 import { convertErrorResponseData } from './parse';
 import { getStorage, setStorage, deleteStorage, setAndDeleteStorage, getOrCreateStorage } from './local.storage';
 
 export {
+  alert,
   getStorage,
   setStorage,
   deleteStorage,
