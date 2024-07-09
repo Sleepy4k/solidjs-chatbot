8709f8aa0fcee66e20945ae4b504bf3c1fadfc8e
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-07-09 14:03:24 +0700

    chore(const): add check status api url

--- src/consts/app.config.ts
+++ src/consts/app.config.ts
@@ -5,6 +5,7 @@ const BING_AI_API_URL = import.meta.env.VITE_BING_AI_API_URL || 'http://localhos
 const GPT_AI_API_URL = import.meta.env.VITE_GPT_AI_API_URL || 'http://localhost:3001';
 const GEMINI_AI_API_URL = import.meta.env.VITE_GEMINI_AI_API_URL || 'http://localhost:3002';
 const BARD_AI_API_URL = import.meta.env.VITE_BARD_AI_API_URL || 'http://localhost:3003';
+const CHECK_STATUS_API_URL = import.meta.env.VITE_CHECK_STATUS_API_URL || 'http://localhost';
 
 export {
   APP_NAME,
@@ -12,5 +13,6 @@ export {
   BING_AI_API_URL,
   GPT_AI_API_URL,
   GEMINI_AI_API_URL,
-  BARD_AI_API_URL
+  BARD_AI_API_URL,
+  CHECK_STATUS_API_URL
 };
